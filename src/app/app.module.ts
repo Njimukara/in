9c6a182e5e5da9cd14a6bd012cdabde64d8fbd54
Njimukara <njimukarabrian@gmail.com>
2020-09-25 10:53:09 +0100

    Database, Routes and Office sign in

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,7 +9,7 @@ import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppmaterialModule } from './appmaterial/appmaterial.module';
 import { AppBootstrapModule } from './app-bootstrap/app-bootstrap.module';
-import { LoginComponent } from './components/registration/login/login.component';
+import { LoginComponent } from './components/login/login.component';
 import { ResultsComponent } from './components/results/results.component';
 import { RoutingModule } from './routing/routing.module';
 import { ToolbarComponent } from './navigation/toolbar/toolbar.component';
--- src/app/routing/routing.module.ts
+++ src/app/routing/routing.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { LoginComponent } from '../components/registration/login/login.component'
+import { LoginComponent } from '../components/login/login.component'
 import { ResultsComponent } from '../components/results/results.component'
 import { DetailsComponent } from '../components/details/details.component'
 import { RouterModule, Routes } from '@angular/router'
